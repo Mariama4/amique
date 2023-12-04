149ca55442bd7a00037403db8d25bdcb6ff4bf0d
mariama4 <mariama4@mail.ru>
2023-12-05 01:32:37 +0300

    update bot error messages

--- src/bot/bot.constants.ts
+++ src/bot/bot.constants.ts
@@ -1,3 +1,22 @@
-export const ALREADY_REGISTERED_ERROR = 'Такой бот уже был зарегистрирован';
+export const BOT_ALREADY_CREATED_ERROR = {
+	error: 'Такой бот уже есть',
+	message: 'Бот с таким именем уже был создан у данного пользователя',
+};
+
+export const BOT_NOT_CREATED_ERROR = {
+	error: 'Бота не существует',
+	message: 'Данного бота не существует у пользователя',
+};
+
+export const FRAME_ALREADY_CREATED_ERROR = {
+	error: 'Такой фрейм уже был создан',
+	message: 'Фрейм с таким именем уже был создан у данного бота',
+};
+
+export const FRAME_NOT_CREATED_ERROR = {
+	error: 'Такой фрейм не существует',
+	message: 'Фрейм с таким именем не существует у данного бота',
+};
+
 export const SAME_STATUS_ERROR = 'Бот уже имеет данный статус';
 export const UNEXPECTED_CREATE_BOT_ERROR = 'Непредвиденная ошибка при создании бота';
