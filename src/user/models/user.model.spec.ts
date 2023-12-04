6ef786ecbc6ad2f9f557d05b922f599191bb04db
mariama4 <mariama4@mail.ru>
2023-12-04 17:36:34 +0300

    update user model

--- src/user/models/user.model.spec.ts
+++ src/user/models/user.model.spec.ts
@@ -1,7 +1,7 @@
 import { UserModel } from './user.model';
 
 describe('UserModel', () => {
-  it('should be defined', () => {
-    expect(new UserModel()).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(new UserModel()).toBeDefined();
+	});
 });
--- src/user/models/user.model.ts
+++ src/user/models/user.model.ts
@@ -3,13 +3,16 @@ import { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<UserModel>;
 
-@Schema()
+@Schema({ collection: 'users', timestamps: true })
 export class UserModel {
 	@Prop({ required: true })
 	email: string;
 
 	@Prop({ required: true })
 	passwordHash: string;
+
+	@Prop({ default: 'USER', enum: ['ADMIN', 'USER'] })
+	role: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(UserModel);
