9eb897ffe78b36483bff3e4cd07b439c9b5b9d4c
mariama4 <mariama4@mail.ru>
2023-12-04 17:36:51 +0300

    update mongodb connection config

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -4,9 +4,13 @@ import { MongooseModuleOptions } from '@nestjs/mongoose';
 export const getMongoConfig = async (
 	configService: ConfigService,
 ): Promise<MongooseModuleOptions> => {
+	console.log({
+		uri: getMongoString(configService),
+		...getMongoOptions(configService),
+	});
 	return {
 		uri: getMongoString(configService),
-		...getMongoOptions(),
+		...getMongoOptions(configService),
 	};
 };
 
@@ -14,15 +18,14 @@ const getMongoString = (configService: ConfigService) =>
 	'' +
 	configService.get('DB_TYPE') +
 	'://' +
-	// find solution for this wtf
-	//configService.get('DB_USER') +
-	//':' +
-	//configService.get('DB_PASSWORD') +
-	//'@' +
+	configService.get('DB_USER') +
+	':' +
+	configService.get('DB_PASSWORD') +
+	'@' +
 	configService.get('DB_HOST') +
 	':' +
-	configService.get('DB_PORT') +
-	'/' +
-	configService.get('DB_NAME');
+	configService.get('DB_PORT');
 
-const getMongoOptions = () => ({});
+const getMongoOptions = (configService: ConfigService) => ({
+	dbName: configService.get('DB_NAME'),
+});
