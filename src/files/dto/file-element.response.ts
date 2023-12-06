f60f0fda7556fd53f29c1929dba7b7fa27e62020
mariama4 <mariama4@mail.ru>
2023-12-06 16:35:05 +0300

    update file module

--- src/files/dto/file-element.response.ts
+++ src/files/dto/file-element.response.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, IsUrl, isURL } from 'class-validator';
+import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
 
 export class FileElementResponse {
 	@IsNotEmpty()
