4c0909d1dbc70ed6dbdd1423f45abb9301297d14
mariama4 <mariama4@mail.ru>
2023-12-07 00:43:48 +0300

    update unixsocket module

--- src/unixsocket/unixsocket.module.ts
+++ src/unixsocket/unixsocket.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UnixsocketService } from './unixsocket.service';
+import { BotModule } from 'src/bot/bot.module';
 
 @Module({
 	providers: [UnixsocketService],
+	imports: [forwardRef(() => BotModule)],
 	exports: [UnixsocketService],
 })
 export class UnixsocketModule {}
--- src/unixsocket/unixsocket.service.ts
+++ src/unixsocket/unixsocket.service.ts
@@ -1,13 +1,17 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit, forwardRef } from '@nestjs/common';
 import { Server, Socket } from 'net';
 import { path } from 'app-root-path';
 import { SendEventDto } from './dto/send-event.dto';
+import { BotService } from 'src/bot/bot.service';
+import { spawn } from 'node:child_process';
 
 @Injectable()
 export class UnixsocketService implements OnModuleInit {
 	private readonly socketPath = `${path}/temp/bots.sock`;
 	private server: Server;
 	private clients: Map<string, Socket> = new Map();
+
+	constructor(@Inject(forwardRef(() => BotService)) private readonly botService: BotService) {}
 
 	onApplicationShutdown(signal: string) {
 		this.server.close();
@@ -17,15 +21,56 @@ export class UnixsocketService implements OnModuleInit {
 		this.server = this.createServer();
 	}
 
+	private deleteClientBySocket(socket: Socket) {
+		const botId = [...this.clients.entries()].filter(({ 1: v }) => v === socket).map(([k]) => k)[0];
+		const isDeleted = this.clients.delete(botId);
+
+		return { botId, isDeleted };
+	}
+
 	private createServer(): Server {
 		const server = new Server();
 
 		server.on('connection', (socket: Socket) => {
-			socket.on('data', (data) => {
+			socket.on('connect', () => {
+				console.log('connect ' + this.clients.size);
+			});
+
+			socket.once('data', (data) => {
+				//const message = data.toString();
+				//console.log(`Bot message: ${message}`);
 				const botId = data.toString();
 				this.clients.set(botId, socket);
+
 				console.log(`Bot #${botId} connected`);
+
 				// TODO: отправление боту его схемы данных
+				//const botFrames = this.botService.findAllFramesByBotId(botId);
+				//const message = JSON.stringify(botFrames);
+				const message = JSON.stringify({
+					a: 'f',
+					b: 'h',
+				});
+				socket.write(`${message}\n\r`);
+				console.log(`Bot #${botId} send frames`);
+				//socket.write('SHUTDOWN');
+				//console.log(`Bot #${botId} send SHUTDOWN`);
+				//console.log(this.clients.size);
+			});
+
+			socket.on('close', (hadError) => {
+				const result = this.deleteClientBySocket(socket);
+				console.log(`Bot #${result.botId} deleted from clients`);
+				console.log('hadError ' + hadError);
+				//console.log(this.clients.size);
+			});
+
+			socket.on('error', (error) => {
+				// TODO: перепроверить (вообще все перепроверить)
+				const result = this.deleteClientBySocket(socket);
+				console.log(`Bot #${result.botId} deleted from clients`);
+				console.log('error ' + error);
+				//console.log(this.clients.size);
 			});
 		});
 
